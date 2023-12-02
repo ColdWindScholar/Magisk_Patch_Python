a65a81b4b6d0e3266d055686913dfe3f5c82307c
ColdWindScholar <3590361911@qq.com>
2023-12-03 01:31:27 +0800

    init

--- log.py
+++ log.py
@@ -1,27 +1,16 @@
 from time import strftime
 
 
-def LOG(info):
-    print('[%s] %s\n' % (strftime('%H:%M:%S'), info))
-
-
-def LOGI(info):
-    print('[%s] \033[94m[INFO]\033[0m%s\n' % (strftime('%H:%M:%S'), info))
-
-
 def LOGE(info):
     print('[%s] \033[91m[ERROR]\033[0m%s\n' % (strftime('%H:%M:%S'), info))
 
 
 def LOGW(info):
-    print('[%s] \033[93m[WARNING]\033[0m%s\n' % (strftime('%H:%M:%S'), info))
+    print('\033[93m[%s]%s\033[0m\n' % (strftime('%H:%M:%S'), info))
 
 
 def LOGS(info):
     print('\033[92m[%s]%s\033[0m\n' % (strftime('%H:%M:%S'), info))
-
-
-def ysuc(info): print(f"\033[32m[{strftime('%H:%M:%S')}]{info}\033[0m")
 
 
 def yecho(info): print(f"\033[36m[{strftime('%H:%M:%S')}]{info}\033[0m")
