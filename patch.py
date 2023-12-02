93f48cdb2230e50ca12db612e80d571a67c9910b
ColdWindScholar <3590361911@qq.com>
2023-12-03 01:18:48 +0800

    i

--- patch.py
+++ patch.py
@@ -1,14 +1,5 @@
 import sys
-
 import os
 from Magisk import Magisk_patch
-
-local = os.getcwd()
-
-
-def patch(boot_img):
-    Magisk_patch(os.path.abspath(boot_img), os.path.join(local, 'bin', 'Magisk')).auto_patch()
-
-
 if __name__ == '__main__':
-    patch(sys.argv[1])
+    Magisk_patch(os.path.abspath(sys.argv[1]), os.path.join(os.getcwd(), 'bin', 'Magisk')).auto_patch()
