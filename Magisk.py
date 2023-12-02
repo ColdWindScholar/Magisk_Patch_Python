4156906acfe5d6f25f34c61d2966c93eb4d7bd51
ColdWindScholar <3590361911@qq.com>
2023-12-03 01:12:32 +0800

    i

--- Magisk.py
+++ Magisk.py
@@ -29,6 +29,9 @@ class Magisk_patch:
             print("Warn:Cannot be named after the generated file name")
             print(f'Please Rename {self.boot_img}')
             sys.exit(1)
+        if not os.path.exists(self.boot_img) or not os.path.exists(self.magiskboot):
+            print("Cannot Found Boot.img or Not Support Your Device")
+            sys.exit(1)
         self.unpack()
         self.check()
         self.patch()
