c8ba9cdff798eb8361fc37c25c6925f34690b385
ColdWindScholar <3590361911@qq.com>
2023-12-03 11:40:18 +0800

    init

--- patch.py
+++ patch.py
@@ -4,6 +4,9 @@ from Magisk import Magisk_patch
 if __name__ == '__main__':
     if sys.argv.__len__() < 2:
         print("参数不足")
-        print("用法:./patch [boot.img]")
-    else:
+        print("默认修补:./patch [boot.img]")
+        print("自定义修补:./patch [boot.img] [Magisk.apk]")
+    elif sys.argv.__len__() == 2:
         Magisk_patch(os.path.abspath(sys.argv[1]), os.path.join(os.getcwd(), 'bin', 'Magisk')).auto_patch()
+    elif sys.argv.__len__() == 3:
+        Magisk_patch(os.path.abspath(sys.argv[1]), os.path.join(os.getcwd(), 'bin', 'Magisk'), MAGISAPK=sys.argv[2]).auto_patch()
