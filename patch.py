650426c18f6ffa4c955987cf0da8ee9639c07f43
ColdWindScholar <3590361911@qq.com>
2023-12-03 10:33:06 +0800

    init

--- patch.py
+++ patch.py
@@ -2,4 +2,8 @@ import sys
 import os
 from Magisk import Magisk_patch
 if __name__ == '__main__':
-    Magisk_patch(os.path.abspath(sys.argv[1]), os.path.join(os.getcwd(), 'bin', 'Magisk')).auto_patch()
+    if sys.argv.__len__() < 2:
+        print("参数不足")
+        print("用法:./patch [boot.img]")
+    else:
+        Magisk_patch(os.path.abspath(sys.argv[1]), os.path.join(os.getcwd(), 'bin', 'Magisk')).auto_patch()
